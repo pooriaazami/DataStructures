d9ae5dd673993884610da4234883b34c135b4aec
Pooria Azami <pooriaazami@gmail.com>
2021-02-07 13:14:05 +0330

    problem fixing

--- unit_tests/Models/Person.cpp
+++ unit_tests/Models/Person.cpp
@@ -1,41 +1,56 @@
 #include "Person.hpp"
-#include <iostream>
 
-class Person : public Person {
+class Person : public Person
+{
 public:
     // Constructor
-    Person(std::string Name, std::string Family, int Age, SEX Sex) {
-        name = Name;
-        family = Family;
-        age = Age;
-        sex = Sex;
+
+    Person()
+    {
+        name = NULL;
+        family = NULL;
+        age = 0;
+        sex = NULL;
     }
 
-    std::string get_name() {
+    std::string get_name()
+    {
         return name;
     }
 
-    void set_name(std::string name) {
-        name = name;
+    void set_name(std::string name)
+    {
+        this->name = name;
     }
 
-    std::string get_family() {
+    std::string get_family()
+    {
         return family;
     }
 
-    void set_family(std::string family) {
-        family = family;
+    void set_family(std::string family)
+    {
+        this->family = family;
     }
 
-    int get_age() {
+    int get_age()
+    {
         return age;
     }
 
-    void set_age(int age) {
-        age = age;
+    void set_age(int age)
+    {
+        this->age = age;
     }
 
-    SEX get_sex() {
+    SEX get_sex()
+    {
         return sex;
     }
+
+private:
+    std::string name;
+    std::string family;
+    std::string age;
+    SEX sex;
 };
